9f0efe81947c9da0c50e7a014c903c6b0170899f
Vitaly Senokosov <6545415@gmail.com>
2019-05-12 20:27:27 +0300

    brain-even refactored

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,6 +2,12 @@
 import name from '..';
 import readlineSync from 'readline-sync';
 
+const randomCeiling = 100;
+const correctAnswersEnough = 3;
+
+const random = () => Math.floor(Math.random() * randomCeiling);
+const getCorrectAnswer = n => ((n % 2 === 0) ? 'yes' : 'no');
+
 console.log(`Hello, ${name}!`);
 
 const checkAnswer = (userAnswer, correctAnswer) => {
@@ -15,18 +21,15 @@ const checkAnswer = (userAnswer, correctAnswer) => {
 const quest = (n) => {
   console.log(`Question: ${n}!`);
   const userAnswer = readlineSync.question('Your answer: ');
-  return (n % 2 === 0)
-    ? checkAnswer(userAnswer, 'yes')
-    : checkAnswer(userAnswer, 'no');
+  return checkAnswer(userAnswer, getCorrectAnswer(n));
 };
 
 const repeat = (acc) => {
-  if (acc === 3) {
+  if (acc === correctAnswersEnough) {
     console.log(`Congratulations, ${name}!`);
     return;
   }
-
-  repeat(acc + quest(Math.floor(Math.random() * 10)));
+  repeat(acc + quest(random()));
 };
 
 repeat(0);
